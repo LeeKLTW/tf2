be9dfa23ab14e028a9f6a2c9f8e25370413bc5e4
LeeKLTW <LeeKLTW@gmail.com>
2019-05-18 11:31:07 +0800

    casting

--- cifar_toy.py
+++ cifar_toy.py
@@ -61,6 +61,8 @@ def main(epochs):
 
     # small dataset test
     (x_train, y_train), (x_test, y_test) = (x_train[:10000], y_train[:10000]), (x_test[:1000], y_test[:1000])
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
 
     x_train /= 255
     x_test /= 255
